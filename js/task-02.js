bf746961ce3e8db232c68f9604e0e902d7f70a61
Denis-isnotavailable <92397311+Denis-isnotavailable@users.noreply.github.com>
2022-08-09 17:21:13 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// 1 Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// 2 Добавит название ингредиента как его текстовое содержимое.
+// 3 Добавит элементу класс item.
+// 4 После чего вставит все <li> за одну операцию в список ul#ingredients.
+
+
+const itemsEl = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+    itemEl.classList.add('item');
+    itemEl.textContent = ingredient;    
+    return itemEl;
+  });
+
+document.querySelector('#ingredients').append(...itemsEl);
